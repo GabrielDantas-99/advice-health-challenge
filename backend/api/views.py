b6433e6152b3f8f9dfd4f44467c30c2f1c941d2d
Gabriel Dantas <gabrieldantaslp@gmail.com>
2025-08-02 20:08:17 -0300

    backend: Criando endpoints para cadastro e acesso de usuario com jwt tokens

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.contrib.auth.models import User
+from rest_framework import generics
+
 # Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
